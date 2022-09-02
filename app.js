dea26889fce62c8fe71c748016152a0e6c78330a
Nicolas Capdevielle <32545380+nn11kk00@users.noreply.github.com>
2022-09-02 11:29:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var reel = document.getElementById("img-reel").style.display = 'block';
+var iframe = document.getElementById("projectplayer").style.display = 'none';
+
+function ocultar() {
+  var reel = document.getElementById("img-reel").style.display = 'none';
+  var iframe = document.getElementById("projectplayer").style.display = 'block';
+
+  var divIframe = document.getElementById("div-iframe").style.padding = '50% 0 0 0';
+  return
+}
